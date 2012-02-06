315c52f2a4e0158c96a1beb0993999c1a99a8669
Rich Lane <rlane@club.cc.cmu.edu>
2012-02-06 01:09:18 -0800

    handle CreateTag and DestroyTag

--- script.lua
+++ script.lua
@@ -27,13 +27,23 @@ local event_iter = fs:iread("/event")
 for event in event_iter do
 	print("event:", event)
 	local start, finish, e = event:find('^(%w+)')
+	local rest = event:sub(finish+2)
 
 	if e == 'Key' then
-		local key = event:sub(finish+2)
+		local key = rest
 		if key == 'Mod1-a' then
+			fs:write("/ctl", "view a")
+		elseif key == 'Mod1-b' then
+			fs:write("/ctl", "view b")
 		else
 			print("unexpected key " .. key)
 		end
+	elseif e == 'CreateTag' then
+		local tag = rest
+		fs:create("/lbar/tag:" .. tag, tag)
+	elseif e == 'DestroyTag' then
+		local tag = rest
+		fs:remove("/lbar/tag:" .. tag)
 	else
 		print("unexpected event " .. e)
 	end
